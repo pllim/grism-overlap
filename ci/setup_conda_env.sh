1eeba07751756aaaf035ff9900367bd44126c019
Joe Filippazzo <jfilippazzo@stsci.edu>
2022-04-14 15:56:14 -0400

    Resetting CI workflow

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Creating base conda environment for Python $PYTHON_VERSION"
-conda create --yes --prefix /home/miniconda3/envs python=$PYTHON_VERSION
-conda activate /home/miniconda3/envs
+conda create --yes --prefix /home/travis/envs python=$PYTHON_VERSION
+conda activate /home/travis/envs
 conda install sphinx numpy flask
 
-echo "Creating grism_overlap conda environment for Python $PYTHON_VERSION"
+echo "Creating ExoCTK conda environment for Python $PYTHON_VERSION"
 conda env update -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=grism_overlap-$PYTHON_VERSION
 source activate $CONDA_ENV
